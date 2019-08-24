fa63e4cad935432f823e1d942911bb0f3da89a3f
jessicalkavanagh <53515616+jessicalkavanagh@users.noreply.github.com>
2019-08-24 09:06:48 +0800

    Add files via upload

---
+++ creditCard.py
@@ -0,0 +1,61 @@
+def calculate_credit_card_number_check_digit(card_no):
+    total = 0
+    # start from last digit from credit card number
+    for position,digit in enumerate(card_no[::-1]):
+        num_digit = int(digit) 
+        # position is even as credit card digit has a missing number
+        if position % 2 == 0:
+            multiply_digit_2 = num_digit*2
+            str_multiply_digit_2 = str(multiply_digit_2)
+            if multiply_digit_2 > 9:
+                subtotal = 0
+                # loop through 2 digits to get subtotal to add to total
+                for second_digit in str_multiply_digit_2:
+                    num_second_digit = int(second_digit)
+                    subtotal += num_second_digit
+                total += subtotal
+            # add 2*digit for every other digit from the digit after check
+            else:
+                total += multiply_digit_2
+        # add digit for every other digit from check
+        else:
+            total += num_digit
+        #formula to calculate check digit
+        if position == len(card_no)-1:
+            sum = total * 9
+            return (str(sum)[-1:])
+
+# print(calculate_credit_card_number_check_digit('542418027979176'))
+
+
+def validate_credit_card_number_check_digit(card_no):
+    total = 0
+    # start from last digit from credit card number
+    for position,digit in enumerate(card_no[::-1]):
+        num_digit = int(digit)
+        # position is even as credit card digit has a missing number
+        if position % 2 != 0:
+            multiply_digit_2 = num_digit*2
+            str_multiply_digit_2 = str(multiply_digit_2)
+            if multiply_digit_2 > 9:
+                subtotal = 0
+                # loop through 2 digits to get subtotal to add to total
+                for second_digit in str_multiply_digit_2:
+                    num_second_digit = int(second_digit)
+                    subtotal += num_second_digit
+                total += subtotal
+            # add 2*digit for every other digit from the digit after check
+            else:
+                total += multiply_digit_2
+        # add digit for every other digit from check
+        else:
+            total += num_digit
+        #formula to calculate check digit
+        if position == len(card_no)-1:
+            sum = total * 9
+            if sum % 10 == 0:
+                return ('This is a valid credit card number.')
+            else:
+                return ('This is an invalid credit card number.')
+
+# print(validate_credit_card_number_check_digit('5424180279791762'))
---
+++ isbn10.py
@@ -0,0 +1,55 @@
+def sum(isbn):
+  total=0
+  for position, digit in enumerate(isbn):
+         num_digit = int(digit)
+         # multiply first digit by 10, second by 9..etc
+         total += (num_digit*(len(isbn)-position))
+         return total 
+
+
+def calculate_isbn10_barcode_check_digit(isbn):
+    total = 0
+    for position, digit in enumerate(isbn,-1):
+        num_digit = int(digit)
+        # multiply first digit by 10, second by 9..etc
+        total += (num_digit*(len(isbn)-position))
+    # return X if check digit is 10
+    if total % 11 == 1:
+        return('X')
+    else:
+        return(str(11-total % 11))
+
+# print(calculate_isbn10_barcode_check_digit('123456789'))
+
+
+def validate_isbn10_barcode_check_digit(isbn):
+
+    isbn_new = list(isbn)
+    total = sum(isbn)
+  
+    if isbn_new[9] == 'X':
+        isbn_new[9] = '0'
+    if total1 % 11 == 10 or total1 % 11 == 0:
+        return('This is a valid isbn10 barcode.')
+    else:
+        return('This is an invalid isbn10 barcode.')
+
+
+
+# def validate_isbn10_barcode_check_digit(isbn):
+
+#     total = 0
+#     isbn_new = list(isbn)
+#     if isbn_new[9] == 'X':
+#         isbn_new[9] = '0'
+#     for position, digit in enumerate(isbn_new):
+#         num_digit = int(digit)
+#         # multiply first digit by 10, second by 9..etc
+#         total += (num_digit*(len(isbn)-position))
+#     # formula to check if isbn10 is correct
+#     if total % 11 == 10 or total % 11 == 0:
+#         return('This is a valid isbn10 barcode.')
+#     else:
+#         return('This is an invalid isbn10 barcode.')
+
+# print(validate_isbn10_barcode_check_digit('188879997X'))
